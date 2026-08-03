105374e87ba1029b4b6028024711b20680aa7951
jklashkari0-beep <jklashkari0@gmail.com>
2026-08-03 19:00:26 +0530

    Fix Gmail SMTP for Render

--- utils/mailer.js
+++ utils/mailer.js
@@ -11,16 +11,18 @@ async function sendSOSEmail({
   if (!to) return;
 
   try {
-    console.log("Sending SOS email to:", to);
-
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
     await transporter.sendMail({
       from: {
         name: "SafeSignal SOS",
