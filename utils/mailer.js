24a6411739d681003104afa18a071ac1832f3523
jklashkari0-beep <jklashkari0@gmail.com>
2026-08-03 21:41:57 +0530

    Force IPv4 for Gmail SMTP

--- utils/mailer.js
+++ utils/mailer.js
@@ -22,8 +22,8 @@ async function sendSOSEmail({
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
